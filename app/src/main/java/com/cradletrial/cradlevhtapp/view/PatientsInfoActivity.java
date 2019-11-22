4802f393f15b9f16f2d83acbbf6f5cc92edef6c4
yla382 <yoonhong_lee>
2019-11-22 04:53:36 -0800

    Its 5am in the morning.....viewing summary filtered by patients almost done. Bug found in implementing retestAnalysis

--- app/src/main/java/com/cradletrial/cradlevhtapp/view/PatientsInfoActivity.java
+++ app/src/main/java/com/cradletrial/cradlevhtapp/view/PatientsInfoActivity.java
@@ -1,9 +1,15 @@
 package com.cradletrial.cradlevhtapp.view;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -14,9 +20,16 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.cradletrial.cradlevhtapp.R;
+import com.cradletrial.cradlevhtapp.model.GestationalAgeUnit;
 import com.cradletrial.cradlevhtapp.model.Patient;
 import com.cradletrial.cradlevhtapp.model.Reading;
+import com.cradletrial.cradlevhtapp.model.ReadingAnalysis;
+import com.cradletrial.cradlevhtapp.model.ReadingManager;
+import com.cradletrial.cradlevhtapp.model.ReadingRetestAnalysis;
 import com.cradletrial.cradlevhtapp.model.WebReading;
+import com.cradletrial.cradlevhtapp.utilitiles.DateUtil;
+import com.cradletrial.cradlevhtapp.view.ui.reading.BaseFragment;
+import com.cradletrial.cradlevhtapp.viewmodel.ReadingAnalysisViewSupport;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.json.JSONArray;
@@ -24,9 +37,16 @@ import org.json.JSONArray;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PatientsInfoActivity extends AppCompatActivity {
+import javax.inject.Inject;
+
+import static com.cradletrial.cradlevhtapp.model.ReadingAnalysis.analyze;
+import static java.security.AccessController.getContext;
+
+public class PatientsInfoActivity extends AppCompatActivity{
     final List<Reading> readings = new ArrayList<>();
     Patient patient;
+    @Inject
+    ReadingManager readingManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,6 +69,114 @@ public class PatientsInfoActivity extends AppCompatActivity {
         //setReadingSpinner(readings);
     }
 
+    private void setReadingGest(Reading reading) {
+        TextView txt_PatientHeader = (TextView) findViewById(R.id.txt_PatientHeader);
+        String unit = "";
+        if(reading.gestationalAgeUnit.equals(GestationalAgeUnit.GESTATIONAL_AGE_UNITS_MONTHS)) {
+            unit = "m";
+        } else if(reading.gestationalAgeUnit.equals(GestationalAgeUnit.GESTATIONAL_AGE_UNITS_WEEKS)){
+            unit = "w";
+        } else {
+            unit = "Not pregnant";
+        }
+
+        txt_PatientHeader.setText(patient.getFirstName() + " " + patient.getLastName() + " @ " + reading.gestationalAgeValue + " " + unit);
+    }
+
+    private  void setSymptoms(Reading reading) {
+        TextView txt_Symptoms = (TextView) findViewById(R.id.txt_Symptoms);
+        String symtoms = reading.getSymptomsString();
+
+        if(symtoms.isEmpty()) {
+            txt_Symptoms.setText("No Symptoms");
+        } else {
+            txt_Symptoms.setText(symtoms);
+        }
+    }
+
+//    private void advice(Reading reading) {
+//        ReadingRetestAnalysis retestAnalysis = ReadingRetestAnalysis(reading, readingManager);
+//        String message = "";
+//        if (retestAnalysis.isRetestRecommendedNow()) {
+//            message = getString(R.string.brief_advice_retest_now);
+//        }
+//
+//        else if (retestAnalysis.isRetestRecommendedIn15Min()) {
+//            message = getString(R.string.brief_advice_retest_after15);
+//        }
+//
+//        else {
+//            ReadingAnalysis analysis = retestAnalysis.getMostRecentReadingAnalysis();
+//            message = analysis.getBriefAdviceText(getContext());
+//        }
+//    }
+
+    private void reading_analyze(Reading reading) {
+            TextView tv;
+            ImageView iv;
+            String errorMessage = "";
+
+            LinearLayout layoutReadings = findViewById(R.id.layout_Readings);
+            layoutReadings.removeAllViews();
+
+            View v = View.inflate(getApplicationContext(), R.layout.reading_vitals_with_icons, null);
+            layoutReadings.addView(v);
+
+            // date & condition summary
+            String time = DateUtil.getDateString(reading.dateTimeTaken);
+            ReadingAnalysis analysis = analyze(reading);
+            String analysisText = analysis.getAnalysisText(getApplicationContext());
+            tv = v.findViewById(R.id.txtReadingHeading);
+            tv.setText( getString(R.string.reading_time_summary,
+                    time, analysisText));
+            if (time.length() == 0) {
+                errorMessage += "- date/time of reading" + "\n";
+            }
+
+            // blood pressure
+            tv = v.findViewById(R.id.txtBloodPressure);
+            Integer sys = reading.bpSystolic;
+            Integer dia = reading.bpDiastolic;
+            if (sys != null && dia != null) {
+                tv.setText(getString(R.string.reading_blood_pressure,
+                        sys, dia));
+                tv.setVisibility(View.VISIBLE);
+            } else {
+                errorMessage += "- blood pressure (systolic and/or diastolic)" + "\n";
+                tv.setVisibility(View.GONE);
+            }
+
+            // heart rate
+            tv = v.findViewById(R.id.txtHeartRate);
+            if (reading.heartRateBPM != null) {
+                tv.setText(getString(R.string.reading_heart_rate,
+                        reading.heartRateBPM));
+                tv.setVisibility(View.VISIBLE);
+            } else {
+                errorMessage += "- heart rate" + "\n";
+                tv.setVisibility(View.GONE);
+            }
+
+            // icons
+            iv = v.findViewById(R.id.imageCircle);
+            iv.setImageResource(ReadingAnalysisViewSupport.getColorCircleImageId(analysis));
+
+            iv = v.findViewById(R.id.imageArrow);
+            iv.setImageResource(ReadingAnalysisViewSupport.getArrowImageId(analysis));
+
+            // error messages
+            tv = v.findViewById(R.id.txtReadingErrors);
+            if (errorMessage.length() > 0) {
+                tv.setVisibility(View.VISIBLE);
+                // TODO: put in strings.xml
+                errorMessage = "Missing required patient vitals:" + "\n" + errorMessage;
+                tv.setText(errorMessage);
+            } else {
+                tv.setVisibility(View.GONE);
+            }
+
+    }
+
     private void setReadingSpinner(List<Reading> readings) {
         Spinner spinner = (Spinner) findViewById(R.id.patientReadings);
         List<String> list = new ArrayList<>();
@@ -64,6 +192,22 @@ public class PatientsInfoActivity extends AppCompatActivity {
             ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, list);
             arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             spinner.setAdapter(arrayAdapter);
+
+            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                    Reading reading = readings.get(position);
+                    setReadingGest(reading);
+                    setSymptoms(reading);
+                    reading_analyze(reading);
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parentView) {
+                    // your code here
+                }
+
+            });
         }
     }
 
@@ -87,7 +231,7 @@ public class PatientsInfoActivity extends AppCompatActivity {
                             WebReading webReading = objectMapper.readValue(jsonArray.get(i).toString(), WebReading.class);
                             Reading reading = new Reading(webReading);
                             readings.add(reading);
-                            System.out.println(readings.size());
+                            System.out.println(jsonArray.get(i));
                         }
                     }
                     setReadingSpinner(readings);
