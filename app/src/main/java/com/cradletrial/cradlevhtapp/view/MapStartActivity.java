595dcdfb6f4c73dcd6d685d1e5e0654ea40aaea9
ccervant <ccervant@sfu.ca>
2019-11-22 23:57:49 -0800

    Add new act

---
+++ app/src/main/java/com/cradletrial/cradlevhtapp/view/MapStartActivity.java
@@ -0,0 +1,17 @@
+package com.cradletrial.cradlevhtapp.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cradletrial.cradlevhtapp.R;
+
+public class MapStartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_start);
+
+        System.out.println("hello");
+    }
+}
